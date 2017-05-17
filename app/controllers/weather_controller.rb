2a083750b6b66e012e55604a7916ea4a68b1b54c
Larry Cherry <larry.cherry.0406@gmail.com>
2017-05-16 23:49:52 -0500

    modified the index to get a users location if lat and lon is not provided

--- app/controllers/weather_controller.rb
+++ app/controllers/weather_controller.rb
@@ -1,13 +1,19 @@
 require 'net/http'
 class WeatherController < ApplicationController
   def index
-    # binding.pry
-    puts "hello #{params['lat']}"
-    uri = URI("https://api.darksky.net/forecast/5b30c9a8f2a1996939817d496865c2eb/#{params['lat']},#{params['lon']}")
-    # binding.pry
-    @json = Net::HTTP.get(uri)
-
-    # @json = { "Jane Doe" => 10, "Jim Doe" => 6 }
-    render json: @json
+    ip = request.remote_ip
+    if !params['lat'] || !params['lon']
+      p 'No lat or lon'
+      uri2 = URI("http://ip-api.com/json/#{ip}")
+      @location = JSON.parse(Net::HTTP.get(uri2))
+       params["lon"] = @location["lon"]
+       params["lat"] = @location["lat"]
+    end
+      WeatherRequest.create(ipaddress: ip, lat: params["lat"], lon: params["lon"]
+      uri = URI("https://api.darksky.net/forecast/5b30c9a8f2a1996939817d496865c2eb/#{params['lat']},#{params['lon']}")
+      binding.pry
+      @json = Net::HTTP.get(uri)
+  
+      render json: @json
   end
 end
