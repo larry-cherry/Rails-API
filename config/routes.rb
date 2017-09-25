f5039c8ab89cf78346309e40eb3620b1e9d29ad1
Larry Cherry <larry.cherry.0406@gmail.com>
2017-09-24 23:46:50 -0500

    created twitch route

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
   get 'weather/index'
   
   get 'wiki/index'
+ 
+  get 'twitch/', to: "twitch#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
