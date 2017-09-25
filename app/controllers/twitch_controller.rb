7abc11b735908c7bbfe4b29b6909aa0a6fc8dcdf
Larry Cherry <larry.cherry.0406@gmail.com>
2017-09-25 00:37:54 -0500

    added channel route for twitch api

--- app/controllers/twitch_controller.rb
+++ app/controllers/twitch_controller.rb
@@ -1,7 +1,11 @@
 class TwitchController < ApplicationController
   require 'net/http'
   def index
-    uri = URI("https://api.twitch.tv/kraken/streams/#{params["stream"]}/?client_id=#{ENV["Twitch_Client"]}")
+    if params["stream"] != nil
+      uri = URI("https://api.twitch.tv/kraken/streams/#{params["stream"]}/?client_id=#{ENV["Twitch_Client"]}")
+    elsif params["channel"] != nil
+      uri = URI("https://api.twitch.tv/kraken/channels/#{params["channel"]}/?client_id=#{ENV["Twitch_Client"]}")
+    end
     @json = Net::HTTP.get(uri)
     render json: @json
   end
